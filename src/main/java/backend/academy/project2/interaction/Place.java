65dacf75c86de6f15538116ff6911f7a1fb1e5c4
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-10-20 19:51:59 +0300

    feat: Add enum with possible places of start and goal

---
+++ src/main/java/backend/academy/project2/interaction/Place.java
@@ -0,0 +1,43 @@
+package backend.academy.project2.interaction;
+
+import backend.academy.project2.maze.Coordinate;
+import backend.academy.project2.maze.Maze;
+import org.checkerframework.checker.units.qual.C;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public enum Place {
+    UP_LEFT(new Coordinate(1, 1), "Слева сверху"),
+    UP_RIGHT(new Coordinate(1, ConsoleLogic.widthSize() - 2), "Справа сверху"),
+    DOWN_LEFT(new Coordinate(ConsoleLogic.heightSize() - 2, 1), "Слева снизу"),
+    DOWN_RIGHT(new Coordinate(ConsoleLogic.heightSize() - 2, ConsoleLogic.widthSize() - 2), "Справа снизу");
+
+    private final int id;
+    private final Coordinate coordinate;
+    private final String description;
+
+    Place(Coordinate coordinate, String description) {
+        this.id = ordinal() + 1;
+        this.coordinate = coordinate;
+        this.description = description;
+    }
+
+    public static String getOrderView() {
+        return Arrays.stream(values())
+            .map(c -> String.format("%d) %s", c.id, c.description))
+            .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    public static Coordinate getById(int id) {
+        return Arrays.stream(values())
+            .filter(c -> c.id == id)
+            .map(c -> c.coordinate)
+            .findFirst()
+            .orElse(null);
+    }
+
+    public static boolean contains(String id) {
+        return Arrays.stream(values())
+            .anyMatch(c -> String.valueOf(c.id).equals(id));
+    }
+}
