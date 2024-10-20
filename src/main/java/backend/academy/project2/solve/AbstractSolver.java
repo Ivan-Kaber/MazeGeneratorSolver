4cfd1e66d62daf37177db9c73de25dfb9a903419
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-10-20 15:53:11 +0300

    feat: Add AbstractSolver with common methods

---
+++ src/main/java/backend/academy/project2/solve/AbstractSolver.java
@@ -0,0 +1,50 @@
+package backend.academy.project2.solve;
+
+import backend.academy.project2.maze.Coordinate;
+import backend.academy.project2.maze.Direction;
+import backend.academy.project2.maze.Maze;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class AbstractSolver implements Solver {
+    protected final Maze maze;
+    protected final Coordinate start;
+    protected final Coordinate goal;
+
+    @Override
+    public List<Coordinate> solve() {
+        return List.of();
+    }
+
+    public AbstractSolver(Maze maze, Coordinate start, Coordinate goal) {
+        this.maze = maze;
+        this.start = start;
+        this.goal = goal;
+    }
+
+    protected <T extends AbstractNode<T>> List<Coordinate> constructPath(T node) {
+        List<Coordinate> path = new ArrayList<>();
+        while (node != null) {
+            path.add(node.coordinate());
+            node = node.parent();
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+    protected List<Coordinate> getValidNeighbors(Coordinate coordinate) {
+        List<Coordinate> neighbors = new ArrayList<>();
+
+        for (Direction direction : Direction.values()) {
+            Coordinate newCoordinate = new Coordinate(
+                coordinate.row() + direction.rowOffset(),
+                coordinate.col() + direction.colOffset());
+            if (maze.isWithinBounds(newCoordinate)) {
+                neighbors.add(newCoordinate);
+            }
+        }
+
+        return neighbors;
+    }
+}
