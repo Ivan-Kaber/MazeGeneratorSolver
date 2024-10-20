7743bd4ae2ffa66f90383d0c1ebc46cb8960ab60
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-10-20 16:45:46 +0300

    feat: Add factory for create necessary generator

---
+++ src/main/java/backend/academy/project2/generate/GeneratorFactory.java
@@ -0,0 +1,10 @@
+package backend.academy.project2.generate;
+
+public class GeneratorFactory {
+    public static Generator createGenerator(GeneratorType type) {
+        return switch (type) {
+            case PRIM -> new PrimGenerator();
+            case RECURSIVE_BACKTRACK -> new RecursiveBacktrackGenerator();
+        };
+    }
+}
