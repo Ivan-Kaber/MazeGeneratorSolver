43c687ce9ed66403e73cf6cd4fc31997385dc9f6
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-10-20 15:27:23 +0300

    feat: Add MazeDecorator for add some types of surfaces

---
+++ src/main/java/backend/academy/project2/generate/MazeDecorator.java
@@ -0,0 +1,29 @@
+package backend.academy.project2.generate;
+
+import backend.academy.project2.maze.Cell;
+import backend.academy.project2.maze.Coordinate;
+import backend.academy.project2.maze.Maze;
+import java.util.Random;
+
+public class MazeDecorator {
+    private static final Random RANDOM = new Random();
+    private static final int BARRIER_CHANCE = 10;
+    private static final int BOOST_CHANCE = 10;
+
+    static void addRandomSurfacesToPassages(Maze maze) {
+        for (int row = 0; row < maze.height(); row++) {
+            for (int col = 0; col < maze.width(); col++) {
+                if (maze.getCell(row, col).type() == Cell.Type.PASSAGE) {
+                    maze.setCellType(new Coordinate(row, col), randomSurface());
+                }
+            }
+        }
+    }
+
+    private static Cell.Type randomSurface() {
+        int chance = RANDOM.nextInt(100);
+        if (chance < BARRIER_CHANCE) return Cell.Type.BARRIER;
+        if (chance < (BOOST_CHANCE + BARRIER_CHANCE)) return Cell.Type.BOOST;
+        return Cell.Type.PASSAGE;
+    }
+}
