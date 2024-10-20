9393c086c87c969f925939be8c9e431184a5237e
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-10-20 20:31:29 +0300

    feat: Add some methods to interract with user

--- src/main/java/backend/academy/project2/solve/SolverType.java
+++ src/main/java/backend/academy/project2/solve/SolverType.java
@@ -1,5 +1,35 @@
 package backend.academy.project2.solve;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum SolverType {
-    ASTAR, BFS
+    ASTAR("Алгоритм А*, найдет кратчайший путь, учитывая типы поверхностей."),
+    BFS("Алгоритм BFS, найдет путь в лабиринте.");
+
+    private final int id;
+    private final String description;
+
+    SolverType(String description) {
+        this.id = ordinal() + 1;
+        this.description = description;
+    }
+
+    public static String getOrderView() {
+        return Arrays.stream(values())
+            .map(c -> String.format("%d) %s", c.id, c.description))
+            .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    public static SolverType getById(int id) {
+        return Arrays.stream(values())
+            .filter(c -> c.id == id)
+            .findFirst()
+            .orElse(null);
+    }
+
+    public static boolean contains(String id) {
+        return Arrays.stream(values())
+            .anyMatch(c -> String.valueOf(c.id).equals(id));
+    }
 }
