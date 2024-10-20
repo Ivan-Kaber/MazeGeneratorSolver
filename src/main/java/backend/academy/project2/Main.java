723c190eaeef861eff9f78a2e56cb242c7dcf808
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-10-20 20:37:23 +0300

    feat: Add Main.class

--- src/main/java/backend/academy/project2/Main.java
+++ src/main/java/backend/academy/project2/Main.java
@@ -1,7 +1,28 @@
 package backend.academy.project2;
 
+import backend.academy.project2.generate.Generator;
+import backend.academy.project2.generate.PrimGenerator;
+import backend.academy.project2.generate.RecursiveBacktrackGenerator;
+import backend.academy.project2.interaction.ConsoleInterface;
+import backend.academy.project2.maze.Coordinate;
+import backend.academy.project2.maze.Maze;
+import backend.academy.project2.render.MazeRenderer;
+import backend.academy.project2.render.Renderer;
+import backend.academy.project2.solve.Solver;
+import backend.academy.project2.solve.SolverFactory;
+import backend.academy.project2.solve.SolverType;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+
+@SuppressFBWarnings("DM_DEFAULT_ENCODING")
 public class Main {
     public static void main(String[] args) {
 
+        PrintStream out = new PrintStream(System.out);
+        Scanner scanner = new Scanner(System.in);
+        ConsoleInterface consoleInterface = new ConsoleInterface(out, scanner);
+        consoleInterface.processUserInteraction();
     }
 }
